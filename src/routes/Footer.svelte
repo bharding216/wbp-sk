56df892e45d994bef8d3605d88d4d7300b3ea3e1
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2024-08-19 06:20:48 -0500

    Minor UI updates

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -5,10 +5,74 @@
 </script>
 
 <footer>
-    <div class="py-5 text-center">
-        <div class="row">
-            <div class="col-12">
-                <p class="fw-bold">&copy; <span>{currentYear}</span> Wendorf Beward & Partners, Inc.</p>
+    <div class="py-5 text-center container">
+        <div class="row py-5 mb-4">
+            <div class="col-6 col-md-3 pb-4 pb-md-0 text-center">
+                <a href="/"><img class="mb-3 img-fluid" src="/logo.png" 
+                    width="240" height="50" alt="logo"></a>
+                <p>
+                    Experience, Expertise, and Results - Trust Wendorf Beward & Partners for Your Program Success
+                </p>
+            </div>
+
+            <div class="col-6 col-md-3 pb-4 pb-md-0 text-center">
+                <p style="font-size: 18px; font-weight: bold;">Quick Links</p>
+
+                <a class="nav-link footer-link" aria-current="page" 
+                    href="/about">
+                    About
+                </a>
+
+                <a class="nav-link footer-link" aria-current="page" 
+                    href="/portfolio">
+                    Portfolio
+                </a>
+
+                <a class="nav-link footer-link" aria-current="page" 
+                    href="/contact">
+                    Contact
+                </a>
+            </div>
+
+            <div class="col-6 col-md-3 pb-4 pb-md-0 text-center">
+                <div class="row">
+                    <div class="col-12">
+                        <h3>San Antonio (HQ)</h3>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row mb-2">
+                            <div class="col-12">
+                                <p class="footer-address fw-bold">Program Office</p>
+                                <p class="footer-address">1826 N. Loop 1604 W, Suite 125</p>
+                                <p class="footer-address">San Antonio, TX 78248</p>
+                            </div>
+                        </div>
+
+                        <div class="row mb-2">
+                            <div class="col-12">
+                                <p class="footer-address fw-bold">Business Office</p>
+                                <p class="footer-address">18123 Apache Springs</p>
+                                <p class="footer-address">San Antonio, TX 78259</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="footer-address">(832) 977-9898</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-3 pb-4 pb-md-0 text-center">
+                <h3>New Braunfels</h3>
+                <p class="footer-address">1175 Diretto</p>
+                <p class="footer-address">New Braunfels, TX 78132</p>
+                <p class="footer-address">(830) 310-4883</p>
             </div>
         </div>
     
